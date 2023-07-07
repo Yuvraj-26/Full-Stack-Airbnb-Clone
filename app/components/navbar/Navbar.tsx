6f5603d7217f512236b5e6cb5619dab0d48790b8
Yuvraj <2604yuvraj@gmail.com>
2023-07-07 21:55:55 +0100

    Search style design implemented for Navbar feature

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 import Container from "../Container";
 import Logo from "./Logo";
+import Search from "./Search";
 
 const Navbar = () => {
     return (  
@@ -26,6 +27,7 @@ const Navbar = () => {
                         "
                     >
                         <Logo />
+                        <Search />
                     </div>    
                 </Container>
             </div>
