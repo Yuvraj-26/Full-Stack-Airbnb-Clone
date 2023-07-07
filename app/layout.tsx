c6e561e1a76ca0d1e597cf7b4bb4856bead7f222
Yuvraj <2604yuvraj@gmail.com>
2023-07-07 21:12:07 +0100

    Containers, Navbar, and Logo created

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { Nunito } from "next/font/google";
 
 import './globals.css'
+import Navbar from "./components/navbar/Navbar";
 
 export const metadata: Metadata = {
   title: 'Airbnb',
@@ -18,7 +19,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <Navbar />
+        {children}
+        </body>
     </html>
   )
 }
