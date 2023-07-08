512bd5e20f963d9cf49b490934342085bd2a3d09
Yuvraj <2604yuvraj@gmail.com>
2023-07-08 22:58:29 +0100

    favouriting system, sync function post and delete implemented for route, usefavourites hook created, and toggle favourite function created

---
+++ app/actions/getListings.ts
@@ -0,0 +1,101 @@
+import prisma from "@/app/libs/prismadb";
+
+// API call not required as database can be accessed directly
+// HOME is Server component, so we can create Actions 
+export interface IListingsParams {
+  userId?: string;
+  guestCount?: number;
+  roomCount?: number;
+  bathroomCount?: number;
+  startDate?: string;
+  endDate?: string;
+  locationValue?: string;
+  category?: string;
+}
+
+// Loading listings without need for API calls
+export default async function getListings(
+  params: IListingsParams
+) {
+  try {
+    const {
+      userId,
+      roomCount, 
+      guestCount, 
+      bathroomCount, 
+      locationValue,
+      startDate,
+      endDate,
+      category,
+    } = params;
+
+    let query: any = {};
+
+    if (userId) {
+      query.userId = userId;
+    }
+
+    if (category) {
+      query.category = category;
+    }
+
+    if (roomCount) {
+      query.roomCount = {
+        gte: +roomCount
+      }
+    }
+
+    if (guestCount) {
+      query.guestCount = {
+        gte: +guestCount
+      }
+    }
+
+    if (bathroomCount) {
+      query.bathroomCount = {
+        gte: +bathroomCount
+      }
+    }
+
+    if (locationValue) {
+      query.locationValue = locationValue;
+    }
+
+    if (startDate && endDate) {
+      query.NOT = {
+        reservations: {
+          some: {
+            OR: [
+              {
+                endDate: { gte: startDate },
+                startDate: { lte: startDate }
+              },
+              {
+                startDate: { lte: endDate },
+                endDate: { gte: endDate }
+              }
+            ]
+          }
+        }
+      }
+    }
+
+    // fetch all listings and order listings
+    const listings = await prisma.listing.findMany({
+      where: query,
+      orderBy: {
+        createdAt: 'desc'
+      }
+    });
+    // using safelisting
+    const safeListings = listings.map((listing) => ({
+      ...listing,
+      createdAt: listing.createdAt.toISOString(),
+    }));
+
+    // catch error
+    return safeListings;
+  } catch (error: any) {
+    throw new Error(error);
+  }
+}
