367644561a1e067607e5e81b0fdd6ae3254c8523
Yuvraj <2604yuvraj@gmail.com>
2023-07-08 19:12:00 +0100

    updated Logo, search function, and updated container

--- app/components/Container.tsx
+++ app/components/Container.tsx
@@ -1,32 +1,24 @@
-
-
-// Mark Component as Client Component to resolve hydration issues
 'use client';
 
-// Create Interface
 interface ContainerProps {
-    children: React.ReactNode;
-}
+  children: React.ReactNode
+};
 
-const Container: React.FC<ContainerProps> = ({
-    children 
-}) => {
-    return (  
-        <div
-            className="
-                max-w-[2520px]
-                mx-auto
-                xl:px-20
-                md:px-10
-                sm:px-2
-                px=4
-            "
-        >
-            
-            {children}
-        </div>
-        
-    );
+const Container: React.FC<ContainerProps> = ({ children }) => {
+  return ( 
+    <div // View port options for different screen sizes
+      className="
+        max-w-[2520px]
+        mx-auto
+        xl:px-20 
+        md:px-10
+        sm:px-2
+        px-4
+      "
+    >
+      {children}
+    </div>
+   );
 }
-
+ 
 export default Container;
--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -1,22 +1,21 @@
 'use client';
 
-import Image from 'next/image';
-import { useRouter } from 'next/navigation';
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 
-const Logo = ( ) => {
-    const router = useRouter();
+const Logo = () => {
+  const router = useRouter(); // router from navigation package
 
-
-    return (
-        <Image 
-            alt="Logo"
-            className="hidden md:block cursor-pointer"
-            height="100"
-            width="100"
-            src="/images/logo.png"
-        
-        />
-    )
+  return ( 
+    <Image
+      onClick={() => router.push('/')}
+      className="hidden md:block cursor-pointer" 
+      src="/images/logo.png" 
+      height="100" 
+      width="100" 
+      alt="Logo" 
+    />
+   );
 }
-
+ 
 export default Logo;
