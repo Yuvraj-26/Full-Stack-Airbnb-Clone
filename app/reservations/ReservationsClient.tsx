23235cb23826be35d63f8592ef97f946a2f16eee
Yuvraj <2604yuvraj@gmail.com>
2023-07-09 00:08:57 +0100

    Functionality implemented to allow owner of a listing to view all reservations and cancel guest reservations

---
+++ app/reservations/ReservationsClient.tsx
@@ -0,0 +1,86 @@
+'use client';
+
+import { toast } from "react-hot-toast";
+import axios from "axios";
+import { useCallback, useState } from "react";
+import { useRouter } from "next/navigation";
+
+import { SafeReservation, SafeUser } from "@/app/types"
+;
+import Heading from "@/app/components/Heading";
+import Container from "@/app/components/Container";
+import ListingCard from "@/app/components/listings/ListingCard";
+
+// create interface
+interface ReservationsClientProps {
+  reservations: SafeReservation[],
+  currentUser?: SafeUser | null,
+}
+
+// client validates functionality for cancel guest reservation
+// the owner of a listing can view all bookings on their property
+// only the owner of a listing can delete guest reservation
+// this deletion of reservation will show on the guest account as expected
+const ReservationsClient: React.FC<ReservationsClientProps> = ({
+  reservations,
+  currentUser
+}) => {
+  const router = useRouter();
+  const [deletingId, setDeletingId] = useState('');
+
+  const onCancel = useCallback((id: string) => {
+    setDeletingId(id);
+
+    // handle deletion of reservation
+    // cancel reservation success
+    axios.delete(`/api/reservations/${id}`)
+    .then(() => {
+      toast.success('Reservation cancelled');
+      router.refresh();
+    })
+    // error handling
+    .catch(() => {
+      toast.error('Something went wrong.')
+    })
+    .finally(() => {
+      setDeletingId(''); // remove deleting id
+    })
+  }, [router]);
+
+  return (
+    <Container>
+      <Heading
+        title="Reservations"
+        subtitle="Bookings on your properties"
+      />
+      <div 
+        className="
+          mt-10
+          grid 
+          grid-cols-1 
+          sm:grid-cols-2 
+          md:grid-cols-3 
+          lg:grid-cols-4
+          xl:grid-cols-5
+          2xl:grid-cols-6
+          gap-8
+        "
+      >
+        {reservations.map((reservation: any) => (
+          <ListingCard
+            key={reservation.id}
+            data={reservation.listing}
+            reservation={reservation}
+            actionId={reservation.id}
+            onAction={onCancel}
+            disabled={deletingId === reservation.id}
+            actionLabel="Cancel guest reservation"
+            currentUser={currentUser}
+          />
+        ))}
+      </div>
+    </Container>
+   );
+}
+ 
+export default ReservationsClient;
