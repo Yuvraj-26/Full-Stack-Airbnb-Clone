275ac1d91b86b07cd035813d911581c2d6424c7a
Yuvraj <2604yuvraj@gmail.com>
2023-07-08 21:18:56 +0100

    SafeUser by modifying types to sanitise variable and prevent passing unintended objects

---
+++ app/api/favorites/[listingId]/route.ts
@@ -0,0 +1,76 @@
+import { NextResponse } from "next/server";
+
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import prisma from "@/app/libs/prismadb";
+
+interface IParams {
+  listingId?: string;
+}
+
+// request function POST
+export async function POST(
+  request: Request, 
+  { params }: { params: IParams }
+) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return NextResponse.error();
+  }
+
+  const { listingId } = params;
+
+  if (!listingId || typeof listingId !== 'string') {
+    throw new Error('Invalid ID');
+  }
+
+  let favoriteIds = [...(currentUser.favoriteIds || [])];
+
+  favoriteIds.push(listingId);
+
+  // prisma 
+  const user = await prisma.user.update({
+    where: {
+      id: currentUser.id
+    },
+    data: {
+      favoriteIds
+    }
+  });
+
+  return NextResponse.json(user);
+}
+
+export async function DELETE(
+  request: Request, 
+  { params }: { params: IParams }
+) {
+  const currentUser = await getCurrentUser();
+
+  // return response
+  if (!currentUser) {
+    return NextResponse.error();
+  }
+
+  const { listingId } = params;
+
+  // validation if ID Invalid
+  if (!listingId || typeof listingId !== 'string') {
+    throw new Error('Invalid ID');
+  }
+
+  let favoriteIds = [...(currentUser.favoriteIds || [])];
+
+  favoriteIds = favoriteIds.filter((id) => id !== listingId);
+
+  const user = await prisma.user.update({
+    where: {
+      id: currentUser.id
+    },
+    data: {
+      favoriteIds
+    }
+  });
+
+  return NextResponse.json(user);
+}
